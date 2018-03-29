82ee1d661acb8066e64349e9b1e1053228d4a1a8
zhoupenghui@snsoft.com.cn <zhoupenghui@snsoft.com.cn>
2018-03-29 14:18:36 +0800

    1

--- Application/Admin/Controller/ExeclController.class.php
+++ Application/Admin/Controller/ExeclController.class.php
@@ -1,9 +1,27 @@
 <?php
 namespace Admin\Controller;
 
-class ExeclController extends Common\Controller\ExeclController
+class ExeclController extends \Common\Controller\ExeclController{
     public function index(){
         $this->display();
+    }
+    public function import(){
+        if (IS_POST){
+           var_dump($this->read());
+           exit();
+        }
+    }
+    public function export(){
+        if(IS_POST){
+            $data = array(
+                array(NULL, 2010, 2011, 2012),
+                array('Q1',   12,   15,   21),
+                array('Q2',   56,   73,   86),
+                array('Q3',   52,   61,   69),
+                array('Q4',   30,   32,    0),
+            );
+            $this->write($data);
+        }
     }
 }
 
